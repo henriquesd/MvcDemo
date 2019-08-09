4aa881231933749939aaf21b36b01d8ec0142236
henriquesd <hsd.foz@gmail.com>
2019-08-09 19:12:33 -0300

    Models

--- MvcDemo/Controllers/HomeController.cs
+++ MvcDemo/Controllers/HomeController.cs
@@ -6,41 +6,40 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MvcDemo.Models;
 
-namespace MvcDemo.Controllers
+public class HomeController : Controller
 {
-    [Route("")]
-    [Route("management-clients")]
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        [Route("")]
-        [Route("initial-page")]
-        //[Route("initial-page/{id}")]
-        //[Route("initial-page/{id}/{category}")]
-        //[Route("initial-page/{id}/{category?}")]
-        [Route("initial-page/{id:int}/{category:guid}")]
-        public IActionResult Index(int id, Guid category)
+        var movie = new Movie
         {
-            return View();
+            Title = "Hi",
+            ReleaseDate = DateTime.Now,
+            Category = null,
+            Evaluation = 10,
+            Price = 20000
+        };
+
+        return RedirectToAction("Privacy", movie);
+        //return View();
+    }
+
+    public IActionResult Privacy(Movie movie)
+    {
+        if (ModelState.IsValid)
+        {
+
         }
 
-        [Route("privacy")]
-        [Route("privacy-politic")]
-        public IActionResult Privacy()
+        foreach (var error in ModelState.Values.SelectMany(m => m.Errors))
         {
-            return Json("{'name': 'Henrique'}");
-
-            //var fileBytes = System.IO.File.ReadAllBytes(@"K:\file.txt");
-            //var fileName = "hello.txt";
-            //return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
-
-             // return Content("Anything");
+            Console.WriteLine(error.ErrorMessage);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        [Route("error-found")]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        return View();
+    }
+
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
---
+++ MvcDemo/Models/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcDemo.Models
+{
+    public class Movie
+    {
+        [Key] // Primary key;
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Title field is required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "The Title must be between 3 and 60 characters")]
+        public string Title { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Release date is in incorrect format")]
+        [Required(ErrorMessage = "The Release Date field is required")]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage = "Category in invalid format")]
+        [StringLength(30, ErrorMessage = "Maximum 30 characters"), Required(ErrorMessage = "The Category field is required")]
+        public string Category { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Value between 1 and 1000")]
+        [Required(ErrorMessage = "Inform the Price field")]
+        [Column(TypeName = "decimal(18,2)")] // this specify in the database;
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Inform the Evaluation field")]
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Just numbers is allowed")]
+        public int Evaluation { get; set; }
+    }
+}
